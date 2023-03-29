f9dee049670d80af9f4a299484cad49e1d6341c5
andres-pg9 <andresperez2039@gmail.com>
2023-03-28 22:45:19 -0600

    Scanner palabrasReservadas

--- Code/Scanner.java
+++ Code/Scanner.java
@@ -47,7 +47,36 @@ public class Scanner {
         palabrasReservadas.put("char", TipoToken.CHAR);
         palabrasReservadas.put("long", TipoToken.LONG);
         palabrasReservadas.put("short", TipoToken.SHORT);
+        //Palabras Reservadas
+        palabrasReservadas.put("class", TipoToken.CLASS);
+        palabrasReservadas.put("func", TipoToken.FUNC);
+        palabrasReservadas.put("return", TipoToken.RETURN);
+        palabrasReservadas.put("print", TipoToken.PRINT);
+        palabrasReservadas.put("this", TipoToken.THIS);
+        palabrasReservadas.put("system", TipoToken.SYSTEM);
 
+        //Tokens
+        palabrasReservadas.put("(", TipoToken.PAR_IZQ);
+        palabrasReservadas.put(")", TipoToken.PAR_DER);
+        palabrasReservadas.put("{", TipoToken.LLA_IZQ);
+        palabrasReservadas.put("}", TipoToken.LLA_DER);
+        palabrasReservadas.put(",", TipoToken.COMA);
+        palabrasReservadas.put(".", TipoToken.PUNTO);
+
+        palabrasReservadas.put(";", TipoToken.PUN_COMA);
+        palabrasReservadas.put("-", TipoToken.MENOS);
+        palabrasReservadas.put("+", TipoToken.MAS);
+        palabrasReservadas.put("*", TipoToken.MULT);
+        palabrasReservadas.put("/", TipoToken.DIV);
+        palabrasReservadas.put("!=", TipoToken.DIFERENTE);
+        palabrasReservadas.put("=", TipoToken.IGUAL);
+
+        palabrasReservadas.put("==", TipoToken.DOB_IGUAL);
+        palabrasReservadas.put("<", TipoToken.MENOR);
+        palabrasReservadas.put("<=", TipoToken.MEN_IGUAL);
+        palabrasReservadas.put(">", TipoToken.MAYOR);
+        palabrasReservadas.put(">=", TipoToken.MAY_IGUAL);
+        palabrasReservadas.put("//", TipoToken.COMENT);
     }
 
     Scanner(String source){
